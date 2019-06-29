5ecb25b5243faa9dc8562a1d1ead64847c094409
ch264 <christinavhastenrath@gmail.com>
2019-06-28 19:17:12 -0700

    added create function for documents in firebase

--- React_Social_App/react_social_functions/functions/index.js
+++ React_Social_App/react_social_functions/functions/index.js
@@ -10,7 +10,7 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello World!");
 });
 
-// return data in firebase datastore
+// get documents(data) from firebase
 exports.getShouts = functions.https.onRequest((req, res) => {
 	admin.firestore().collection('shouts').get()
 	.then(data => {
@@ -18,5 +18,27 @@ exports.getShouts = functions.https.onRequest((req, res) => {
 		data.forEach(doc => {
 			shouts.push(doc.data()); // data() is function that returns data inside the document.
 		}) 
+		return res.json(shouts); // return as json
+	})
+	.catch(err => console.error(err))
+})
+
+// creates firebase document
+exports.createShout = functions.https.onRequest((req, res) => {
+	if(req.method !== 'POST'){
+		return res.status(400).json({error: 'Method not allowed, client error'})
+	}
+	const newShout = {
+		body: req.body.body,
+		userHandle: req.body.userHandle,
+		createdAt: admin.firestore.Timestamp.fromDate(new Date())
+	};
+	// add takes json object and adds it to the the database
+	admin.firestore().collection('shouts').add(newShout).then(doc => {
+		res.json({ message: `document ${doc.id} created successfully`})
+		.catch(err => {
+			res.status(500).json({error: `something went wrong`});
+			console.error(err);
+		})
 	})
 })
